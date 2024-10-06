068e413eb1fbc3853fd90358485153d790b6e461
chase coogan <chasecoogan12@gmail.com>
2024-10-06 00:48:06 -0400

    update GBM function to iterate over steps

--- MonteCarloPricing/src/montecarlo/MonteCarloSimulation.py
+++ MonteCarloPricing/src/montecarlo/MonteCarloSimulation.py
@@ -1,36 +1,47 @@
 import random
 import math
 import scipy.stats
+import matplotlib.pyplot as plt
 
 RISK_FREE_INTEREST = .03969 # 10 year treasury
-DELTA_TIME = 1 
+DELTA_TIME = 1 # time to exp / TIME
 TIME = 1 # in years
+STEPS_N = 252
 
 class MonteCarloSimulation:
-    def __init__(self, stock_value, strike, volatility):
+    def __init__(self, stock_value, strike, volatility, steps, simulations):
         self.stock_value = stock_value
         self.risk_free_interest = RISK_FREE_INTEREST 
         self.strike = strike 
         self.sigma = volatility 
-        self.delta_time = DELTA_TIME 
+        self.delta_time = TIME/STEPS_N 
         self.T = TIME
+        self.steps = steps
+        self.simulations = simulations
+        self.payoffs = []
 
     def Geometric_Brownian_Motion(self, option_type="call"):
-        gaussian = random.gauss(0, 1)
-        exponent = ((self.risk_free_interest - 0.5 * self.sigma ** 2) 
-                    * self.delta_time + self.sigma * math.sqrt(self.delta_time) * gaussian)
+        st_delta_t_sum = self.stock_value
+        for _ in range(self.steps):
+        
+            gaussian = random.gauss(0, 1)
+            exponent = ((self.risk_free_interest - 0.5 * self.sigma ** 2) 
+                        * self.delta_time + self.sigma * math.sqrt(self.delta_time) * gaussian)
 
-        st_delta_t = self.stock_value * math.exp(exponent)
+            st_delta_t_sum = st_delta_t_sum * math.exp(exponent)
+            yield (_, st_delta_t_sum)  
 
         if option_type  == "call".lower():
-            payoff = max(st_delta_t - self.strike, 0)
+            payoff = max(st_delta_t_sum - self.strike, 0)
         elif option_type == "put".lower():
-            payoff = max(self.strike - st_delta_t, 0)
+            payoff = max(self.strike - st_delta_t_sum, 0)
         else:
-            raise ValueError    
-        
-        option_price = math.exp(-self.risk_free_interest * self.T) * payoff
-        return option_price
+            raise ValueError 
+
+        self.payoffs.append(payoff)
+
+        # option_price = math.exp(-self.risk_free_interest * self.T) * payoff
+        # return option_price
 
     def Black_Scholes(self, option_type="call".lower()):
         N = scipy.stats.norm.cdf
@@ -47,3 +58,4 @@ class MonteCarloSimulation:
             raise ValueError
         
         return cost
+
